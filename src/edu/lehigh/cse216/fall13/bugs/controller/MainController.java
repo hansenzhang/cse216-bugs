d9a8bd233b8b79a4f26540ee6941ecdd2b2c321f
Hansen Zhang <hansen.s.zhang@gmail.com>
2013-10-31 23:27:07 -0400

    Added main controller methods

--- src/edu/lehigh/cse216/fall13/bugs/controller/MainController.java
+++ src/edu/lehigh/cse216/fall13/bugs/controller/MainController.java
@@ -4,10 +4,35 @@
  */
 package edu.lehigh.cse216.fall13.bugs.controller;
 
+import edu.lehigh.cse216.fall13.bugs.business.Bug;
+import java.util.ArrayList;
+
 /**
  *
  * @author hansen
  */
 public class MainController {
+    MainController() {
+        //default
+    }
     
+    void add (Bug b) {
+    
+    }
+    
+    ArrayList<Bug> list () {
+        return null; //TODO: only for now, I'm not constructing this    
+    }
+    
+    Bug view (int bugId) {
+        return null;
+    }
+    
+    ArrayList<Bug> search(String... strings) {
+        return null;
+    }
+    
+    Bug edit (int bugId) {
+        return null;
+    }
 }
