5fc9183b34863c0f628bab562f5db724e332b40e
cappemic <cappemic@gmail.com>
2013-10-31 23:07:47 -0400

    Update Comment.java

--- src/edu/lehigh/cse216/fall13/bugs/business/Comment.java
+++ src/edu/lehigh/cse216/fall13/bugs/business/Comment.java
@@ -3,6 +3,11 @@ package edu.lehigh.cse216.fall13.bugs.business;
 //import the user class
 import java.util.Date;
 
+/**
+ * This object is used to store the comments that users make on each bug object
+ * 
+ */
+
 public class Comment{
 
     private int bugID;
@@ -10,6 +15,15 @@ public class Comment{
     private String msg;
     private Date date;
     
+    
+    /**
+     * Constructor for the comment object
+     * Intansiates the bug ID, the user who created it, and the body of the comment
+     * 
+     * @param	bugID	int that stores the ID of the bug the comment is for
+     * @param	user	User object that is the user that submitted the comment
+     * @param	msg	String that is the body of the comment
+     */
     public Comment(int bugID, User user, String msg){
 	this.bugID = bugID;
 	this.user = user;
@@ -19,18 +33,30 @@ public class Comment{
 	
     }
 
+    /**
+     * Returns an int that is the bugID 
+     */
     public int getBugID(){
 	return bugID;
     }
     
+    /**
+     * Returns the User object of the user that submitted the comment
+     */
     public User getUser(){
 	return user;
     }
  
+    /**
+     * Returns the body of the Comment
+     */
     public String getMsg(){
 	return msg;
     }
 
+    /**
+     * Returns the Date object, giving the date the comment was created.
+     */
     public Date getDate(){
 	return date;
     }
